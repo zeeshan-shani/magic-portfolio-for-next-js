1614ffdde275fed5189ed4f29b9b2072ebd49f56
Muhammad Zeeshan <59706023+zeeshan-shani@users.noreply.github.com>
2025-04-26 07:08:28 -0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,9 @@
 import { getPosts } from "@/app/utils/utils";
 import { baseURL, routes as routesConfig } from "@/app/resources";
 
+export const revalidate = 0;
+export const dynamic = "force-static";
+
 export default async function sitemap() {
   const blogs = getPosts(["src", "app", "blog", "posts"]).map((post) => ({
     url: `https://${baseURL}/blog/${post.slug}`,
