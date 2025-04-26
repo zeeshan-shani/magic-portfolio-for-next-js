197a093777f29a389c52f96828a4a24c6dd9b564
Muhammad Zeeshan <59706023+zeeshan-shani@users.noreply.github.com>
2025-04-26 07:00:56 -0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,3 +1,20 @@
+// import { baseURL } from "@/app/resources";
+
+// export default function robots() {
+//   return {
+//     rules: [
+//       {
+//         userAgent: "*",
+//       },
+//     ],
+//     sitemap: `${baseURL}/sitemap.xml`,
+//   };
+// }
+
+
+export const revalidate = 0;
+export const dynamic = "force-static";
+
 import { baseURL } from "@/app/resources";
 
 export default function robots() {
@@ -10,3 +27,4 @@ export default function robots() {
     sitemap: `${baseURL}/sitemap.xml`,
   };
 }
+
